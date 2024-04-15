e6fdf32783a60f3a12187401b3fe99920e38e22f
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-04-15 17:05:36 +0100

    cheeck

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,4 +1,7 @@
+// interesting jquery import
 import $ from "jquery";
+
+// add three different p
 $("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
 $("body").append("<p>Copyright - Holberton School</p>");
