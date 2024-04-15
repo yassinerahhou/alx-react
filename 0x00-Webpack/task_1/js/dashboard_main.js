e93a51f5f5451f3e5293533a0d624121fa588e44
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-04-15 17:07:38 +0100

    check

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+// append elements
+$("body").append(`<p>Holberton Dashboard</p>`);
+$("body").append(`<p>Dashboard data for the students</p>`);
+$("body").append(`<button>Click here to get started</button>`);
+$("body").append(`<p id="count"></p>`);
+$("body").append(`<p>Copyright - Holberton School</p>`);
