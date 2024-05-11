029a80394d5e2866aec1bdcbd46f1988aa8218e2
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-05-11 09:39:45 +0100

    CourseListRow component

--- 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,3 +1,40 @@
 import React from "react";
 
 import PropTypes from "prop-types";
+const CourseListRow = ({
+  isHeader = false,
+  textFirstCell,
+  textSecondCell = null,
+}) => {
+  // Check if isHeader is true
+  if (isHeader) {
+    return (
+      <tr>
+        {/* check if textSecondCell is null */}
+        {textSecondCell === null ? (
+          <th colSpan="2">{textFirstCell} </th>
+        ) : (
+          <>
+            <th>{textFirstCell} </th>
+            <th>{textSecondCell} </th>
+          </>
+        )}
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td>{textFirstCell} </td>
+        <td>{textSecondCell} </td>
+      </tr>
+    );
+  }
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
+};
+
+export default CourseListRow;
